d478a79bf9b97b706c53af90d9c1f9a52d2c9a3e
Aaron44201 <44201@longroad.ac.uk>
2014-09-09 12:54:27 +0100

    Hello world with the name input.

---
+++ Hello world input name.py
@@ -0,0 +1,12 @@
+#Aaron Bentley
+#9/9/2014
+#Excersise hello world
+
+#computer says hello world
+print("Hello world")
+#computer asks for your name as an input
+first_name = input("Please enter your name here: ")
+#Prints the name taken as input
+print(first_name)
+#Prints name inside of text taking name from memory
+print("Hi {0}!".format(first_name))
