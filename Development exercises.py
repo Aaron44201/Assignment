545cf828becbb235eb7b3e9468a5f56624f8795c
Aaron44201 <44201@longroad.ac.uk>
2014-09-12 14:17:26 +0100

    task 1 dev exercise

---
+++ Development exercises.py
@@ -0,0 +1,9 @@
+#Aaron Bentley
+#12/09/2014
+#Exercise 1 development exercises
+
+number1 = int(input("Please insert your first number: "))
+number2 = int(input("Please insert your second number: "))
+divison = number1/number2
+remainder = number1 % number2
+print("The answer is {0} and remainder {1}".format (divison, remainder))
