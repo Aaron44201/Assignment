e878794a37e9d47e341420451c650fc30c371f4f
Aaron44201 <44201@longroad.ac.uk>
2014-09-12 14:34:33 +0100

    to do at home

---
+++ assignment 2.py
@@ -0,0 +1,7 @@
+#Aaron Bentley
+#12/09/2014
+#Exercise 2 development exercises
+
+fahrenheit = int(input("What is the tempreature in fahrenheit? "))
+centigrade = (fahrenheit - 32)*(5/9)
+print("centigrade:".format
