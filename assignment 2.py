a278a604c0a9514c455ab2a2d78390fbf820582a
Aaron44201 <44201@longroad.ac.uk>
2014-09-15 18:45:09 +0100

    task 2
    changed it so that it actually works and rounds the number down so that
    the decimal is not huge

--- assignment 2.py
+++ assignment 2.py
@@ -3,5 +3,7 @@
 #Exercise 2 development exercises
 
 fahrenheit = int(input("What is the tempreature in fahrenheit? "))
-centigrade = (fahrenheit - 32)*(5/9)
-print("centigrade:".format
+centigrade = (fahrenheit-32)*(5/9)
+centigrade2 = round(centigrade,1)
+print("The tempreature in centigrade is: {0}".format(centigrade2))
+
