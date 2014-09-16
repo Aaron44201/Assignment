f37ca81458cdf1a37d0baba96cf9d764c6f6130a
Aaron44201 <44201@longroad.ac.uk>
2014-09-16 12:05:46 +0100

    assignment tasks

---
+++ Test scores.py
@@ -0,0 +1,7 @@
+#Aaron Bentley
+#12/09/2014
+#Excersise 2
+
+testA = int(input("Please enter results for test A: "))
+testB = int(input("please enter results for test B: "))
+print("Your combined score is {0}".format (testA + testB))
---
+++ assignment 3.py
@@ -0,0 +1,10 @@
+#Aaron Bentley
+#16/09/2014
+#Exercise 3 development exercises
+
+height_inches = int(input("Please enter your height in inches "))
+weight_stone = int(input("Please enter your weight in stone "))
+height_cm = (height_inches)*2.54
+weight_kg = (weight_stone)*6.364
+print("your height is {0}cm and your weight is {1}kg".format (height_cm,weight_kg))
+
